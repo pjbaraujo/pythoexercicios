8ef26e94b01365e48466a5046b3b79f73738195d
Paulo <pauloaraujo.nsn@gmail.com>
2021-04-17 17:10:59 -0300

    ex002 Gustavo Guanabara youtube
    Faça um programa que leia o nome de uma pessoa e mostre
    uma mensagem de boas-vindas.

---
+++ ex002.py
@@ -0,0 +1,7 @@
+"""
+Faça um programa que leia o nome de uma pessoa e mostre
+uma mensagem de boas-vindas.
+"""
+
+nome = input('Qual o seu nome: ')
+print(f'Olá {nome} seja bem vindo ao mundo Python')
