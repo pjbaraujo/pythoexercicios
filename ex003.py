123fa802225b753388f1291d61a4749da427b7c1
Paulo <pauloaraujo.nsn@gmail.com>
2021-04-17 17:21:20 -0300

    Gustavo Guanabara youtube
    ex003
    Crie um programa que leia dois números e mostre a soma entre eles.

---
+++ ex003.py
@@ -0,0 +1,10 @@
+"""
+ex003
+Crie um programa que leia dois números e mostre a soma entre eles.
+"""
+
+n1 = int(input('Insira o primeiro valor: '))
+n2 = int(input('Insira o segundo valor: '))
+
+soma = n1 + n2
+print(f'O resultado da soma dos valos {n1} e {n2} é: {soma} ')
